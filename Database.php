96e2340fdec481f44ba8dae423dde5cc07a657f3
SlimSoftware <slimsoftware512@gmail.com>
2021-07-21 12:51:58 +0200

    Show error message when db connection fails

--- Database.php
+++ Database.php
@@ -7,9 +7,20 @@ class Database
 
     private function __construct()
     {
-        self::$con = new PDO("mysql:host=" . Config::DB_HOST . ";dbname=" . Config::DB_NAME . ";charset=utf8mb4", Config::DB_USERNAME, Config::DB_PASSWORD);
-        self::$con->setAttribute(PDO::ATTR_EMULATE_PREPARES, false); // needed so we get back the correct datatypes from the db
-        self::$con->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC); // we only need column names from the db, not the default both column names and indexes
+        $host = Config::DB_HOST;
+        $dbName = Config::DB_NAME;
+        $dsn = "mysql:host=$host;dbname=$dbName;charset=utf8mb4";
+
+        try {
+            self::$con = new PDO($dsn, Config::DB_USERNAME, Config::DB_PASSWORD);
+        } catch (PDOException $e) {
+            die("Cannot connect to database");
+        }
+        
+        // Set emulate prepare to false so we get back the correct datatypes (like int) from the db
+        self::$con->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        // We only need column names from the db, not the default both column names and indexes
+        self::$con->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC); 
     }
 
     public static function getInstance()
