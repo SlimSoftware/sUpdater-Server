98dac79c5d8d7ab74ff1b191a95211579ec68f7c
SlimSoftware <slimsoftware512@gmail.com>
2021-07-22 17:56:48 +0200

    Add login page

---
+++ dashboard/login.php
@@ -0,0 +1,37 @@
+<?php
+if (isset($_POST["user"]) && isset($_POST["pass"])) {
+    require __DIR__ . "/../Database.php";
+    $db = Database::getInstance();
+    $stmt = $db->prepare("SELECT password FROM users WHERE username = ?");
+    $stmt->execute([$_POST["user"]]);
+    $hashedPassword = $stmt->fetch(PDO::FETCH_COLUMN);
+
+    if ($hashedPassword !== false) {
+        if (password_verify($_POST["pass"], $hashedPassword)) {
+            header("Location: dashboard/index.php");
+        } else {
+            echo "Username or password incorrect!<br/>";
+        }
+    } else {
+        echo "Server error<br/>";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Log In - sUpdater Server</title>
+</head>
+<body>
+    <h1>Log In</h1>
+    <form method="POST">
+        <p>Username: <input type="text" name="user" /><br />
+        Password: <input type="password" name="pass" /></p>
+        <input type="submit" value="Login" />
+    </form>
+</body>
+</html>
