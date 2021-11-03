676e7b270d57dc05d5cbfd03156df89cb82098da
SlimSoftware <slimsoftware512@gmail.com>
2021-11-03 12:37:04 +0100

    Add detectinfo and installers to import script

--- import.php
+++ import.php
@@ -13,12 +13,11 @@ if (file_exists($xmlPath)) {
         $stmt = $db->prepare("SELECT id FROM apps WHERE name = ?");   
         $stmt->execute([$appName]);
         $appId = $stmt->fetchColumn();
-        $appExists = $stmt->rowCount() > 0;
         $changelogId = null;
         $descriptionId = null;
 
         if ($app->changelog) {
-            if ($appExists) {
+            if ($appId != "") {
                 $stmt = $db->prepare("SELECT changelog_id FROM apps WHERE id = ?");   
                 $stmt->execute([$appId]);
                 $changelogId = $stmt->fetchColumn();
@@ -49,7 +48,7 @@ if (file_exists($xmlPath)) {
         }
 
         if ($app->description) {
-            if ($appExists) {
+            if ($appId != "") {
                 $stmt = $db->prepare("SELECT description_id FROM apps WHERE id = ?");   
                 $stmt->execute([$appId]);
                 $descriptionId = $stmt->fetchColumn();
@@ -80,7 +79,7 @@ if (file_exists($xmlPath)) {
             }
         }
 
-        if ($appExists) {
+        if ($appId != "") {
             $stmt = $db->prepare("UPDATE apps SET name = ?, version = ?, noupdate = ?, changelog_id = ?, description_id = ? WHERE id = ?");   
             $updated = $stmt->execute([$appName, $app->version, $app->type === "noupdate", $changelogId, $descriptionId, $appId]);
             
@@ -91,6 +90,7 @@ if (file_exists($xmlPath)) {
         } else {
             $stmt = $db->prepare("INSERT INTO apps (name, version, noupdate, changelog_id, description_id) VALUES (?, ?, ?, ?, ?)");   
             $added = $stmt->execute([$appName, $app->version, $app->type === "noupdate", null, null]);
+            $appId = $db->lastInsertId();
             
             if ($added)
                 echo "App $appName added to db<br/>";
@@ -98,6 +98,50 @@ if (file_exists($xmlPath)) {
                 echo "Failed to add app $appName to db<br/>";
         }
 
+        $stmt = $db->prepare("SELECT id FROM detectinfo WHERE app_id = ?");   
+        $stmt->execute([$appId]);
+        $detectInfoId = $stmt->fetchColumn();
+        $archs = ["*", "x86", "x64"];
+        
+        if ($detectInfoId != "") {
+            $stmt = $db->prepare("UPDATE detectinfo SET app_id = ?, arch = ?, regkey = ?, regvalue = ?, exepath = ? WHERE id = ?");   
+            $updated = $stmt->execute([$appId, array_search($app->arch, $archs), $app->regkey, $app->regvalue, $app->exepath, $detectInfoId]);
+            
+            if ($updated)
+                echo "Detectinfo for $appName updated in db<br/>";
+            else 
+                echo "Failed to update detectinfo for $appName in db<br/>";
+        } else {
+            $stmt = $db->prepare("INSERT INTO detectinfo (app_id, arch, regkey, regvalue, exepath) VALUES (?, ?, ?, ?, ?)");   
+            $added = $stmt->execute([$appId, array_search($app->arch, $archs), $app->regkey, $app->regvalue, $app->exepath]);
+            
+            if ($added)
+                echo "Detectinfo for $appName added to db<br/>";
+            else 
+                echo "Failed to add detectinfo for $appName to db<br/>";
+        }
+
+        $stmt = $db->prepare("SELECT id FROM installers WHERE app_id = ?");   
+        $stmt->execute([$appId]);
+        $installerId = $stmt->fetchColumn();
+        
+        if ($installerId != "") {
+            $stmt = $db->prepare("UPDATE installers SET app_id = ?, dl = ?, launch_args = ? WHERE id = ?");   
+            $updated = $stmt->execute([$appId, $app->dl, $app->switch, $appId]);
+            
+            if ($updated)
+                echo "Installer for $appName updated in db<br/><br/>";
+            else 
+                echo "Failed to update installer for $appName in db<br/>";
+        } else {
+            $stmt = $db->prepare("INSERT INTO installers (app_id, dl, launch_args) VALUES (?, ?, ?)");   
+            $added = $stmt->execute([$appId, $app->dl, $app->switch]);
+            
+            if ($added)
+                echo "Installer for $appName added to db<br/>";
+            else 
+                echo "Failed to add installer for $appName to db<br/><br/>";
+        }
     }
 
     foreach ($xml->portable as $pApp) {
