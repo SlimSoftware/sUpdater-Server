455db9b86dfd8f0c1171ddb8c2dfdec9b8f3e489
SlimSoftware <slimsoftware512@gmail.com>
2021-12-07 16:13:45 +0100

    Change mobile navbar toggler to white

--- dashboard/inc/nav.php
+++ dashboard/inc/nav.php
@@ -12,7 +12,6 @@ $filename = basename($_SERVER['SCRIPT_NAME']);
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 		<span class="navbar-toggler-icon"></span>
-		<span class="button-label navbar-toggler-label"></span>
 	</button>
 	<div class="collapse navbar-collapse" id="navbarSupportedContent">
 		<ul class="navbar-nav me-auto">
