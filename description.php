882cc08f4842eb066360dc7303852a0c43d0fddb
SlimSoftware <slimsoftware512@gmail.com>
2021-12-13 20:03:46 +0100

    New endpoints for descriptions and changelogs

---
+++ description.php
@@ -0,0 +1,10 @@
+<?php
+if (isset($_GET["id"])) {
+    require __DIR__ . "/Database.php";
+    $db = Database::getInstance();
+    $stmt = $db->prepare("SELECT text FROM descriptions WHERE app_id = ?");
+    $stmt->execute([$_GET["id"]]);
+    $apps = $stmt->fetchAll();
+} else {
+    http_response_code(404);
+}
