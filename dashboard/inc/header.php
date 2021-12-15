ff90e1f93d405b68f0e686d528de4f694a52c2c6
SlimSoftware <slimsoftware512@gmail.com>
2021-12-15 17:05:24 +0100

    Move style.css to dashboard folder

--- dashboard/inc/header.php
+++ dashboard/inc/header.php
@@ -18,7 +18,7 @@ $dir = "/" . Config::SERVER_SUBDIR;
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $title ?> - sUpdater Server</title>
-    <link rel="stylesheet" type="text/css" href="<?= "$dir/style.css" ?>">
+    <link rel="stylesheet" type="text/css" href="<?= "$dir/dashboard/style.css" ?>">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="shortcut icon" href="<?= "$dir/dashboard/img/favicon/favicon.ico" ?>">
 	<link rel="icon" sizes="16x16 32x32 64x64" href="<?= "$dir/dashboard/img/favicon/favicon.ico" ?> ">
