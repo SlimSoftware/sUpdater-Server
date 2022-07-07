1f1f71ed05f54fe11326a2a6378bf4923a3765fc
SlimSoftware <slimsoftware512@gmail.com>
2022-07-07 20:33:03 +0200

    Update to bootstrap 5.2.0-beta1

--- dashboard/inc/header.php
+++ dashboard/inc/header.php
@@ -19,7 +19,7 @@ if (!isset($_SESSION["user"]) && $pageName !== "login.php" && $pageName !== "ins
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $title ?> - sUpdater Server</title>
     <link rel="stylesheet" type="text/css" href="<?= "$dir/dashboard/style.css" ?>">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <link rel="shortcut icon" href="<?= "$dir/dashboard/img/favicon/favicon.ico" ?>">
 	<link rel="icon" sizes="16x16 32x32 64x64" href="<?= "$dir/dashboard/img/favicon/favicon.ico" ?> ">
 	<link rel="icon" type="image/png" sizes="196x196" href="<?= "$dir/dashboard/img/favicon/favicon-192.png" ?>">
