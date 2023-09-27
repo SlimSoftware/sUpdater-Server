c0c084cc164dc436ed991832442874cfb3cb4e14
SlimSoftware <slimsoftware512@gmail.com>
2023-09-27 21:14:11 +0200

    Build script: new way to omit npm dev packages

--- build/build.sh
+++ build/build.sh
@@ -7,6 +7,6 @@ cd build/output
 mv .env.example.prod .env.example
 mv docker-compose.prod.yml docker-compose.yml
 
-npm install --only=prod
+npm install --omit=dev
 npm run build
 composer install --no-dev
