0e12dc62e6ec98ba7916e8d65976c1bc06e249b2
SlimSoftware <slimsoftware512@gmail.com>
2023-10-01 12:21:37 +0200

    Fix %ver.1% and higher variables not working in API

--- app/Helpers/VariableHelper.php
+++ app/Helpers/VariableHelper.php
@@ -8,23 +8,29 @@ class VariableHelper
         if ($version !== null) {
             $downloadLink = str_replace('%ver%', $version, $downloadLink);
             $downloadLink = str_replace('%ver.0%', str_replace('.', '', $version), $downloadLink);
-            $downloadLink = str_replace('%ver.1%', self::splitVersion($version, 2), $downloadLink);
-            $downloadLink = str_replace('%ver.2%', self::splitVersion($version, 3), $downloadLink);
-            $downloadLink = str_replace('%ver.3%', self::splitVersion($version, 4), $downloadLink);
+            $downloadLink = str_replace('%ver.1%', self::splitVersion($version, 1), $downloadLink);
+            $downloadLink = str_replace('%ver.2%', self::splitVersion($version, 2), $downloadLink);
+            $downloadLink = str_replace('%ver.3%', self::splitVersion($version, 3), $downloadLink);
         }
 
         return $downloadLink;
     }
 
-    private static function splitVersion(?string $version, int $digits)
+    private static function splitVersion(?string $version, int $numberOfDots)
     {
         if (!$version) {
             return '';
         }
 
-        $numbers = explode('.', $version, $digits);
-        $newVersion = implode('.', $numbers);
+        // +2 because the last element of the array could contain the rest of the string
+        $numbers = explode('.', $version, $numberOfDots + 2); 
 
+        // remove the last element containing the rest of the string if present
+        if (count($numbers) > $numberOfDots + 1) {
+            array_pop($numbers);
+        }
+
+        $newVersion = implode('.', $numbers);
         return $newVersion;
     }
 }
