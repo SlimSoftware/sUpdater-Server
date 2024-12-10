a51aca0125ec0b63093d0d2756d29fd10acc1612
SlimSoftware <slimsoftware512@gmail.com>
2024-12-10 20:40:27 +0100

    Fix AuthAPIController not found error

--- routes/auth.php
+++ routes/auth.php
@@ -1,13 +1,13 @@
 <?php
 
-use App\Http\Controllers\API\Dashboard\AuthController;
+use App\Http\Controllers\API\Dashboard\AuthAPIController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-    Route::post('login', [AuthController::class, 'login']);
-    Route::get('authenticated', [AuthController::class, 'checkAuthenticated']);
+    Route::post('login', [AuthAPIController::class, 'login']);
+    Route::get('authenticated', [AuthAPIController::class, 'checkAuthenticated']);
 });
 
 Route::middleware('auth')->group(function() {
-    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('logout', [AuthAPIController::class, 'logout']);
 });
