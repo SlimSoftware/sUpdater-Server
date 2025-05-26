0acad2522c155f51f2f16989b7a08f1b4ec93c4d
SlimSoftware <slimsoftware512@gmail.com>
2025-05-26 20:16:18 +0200

    Fix url inputs required

--- resources/js/components/URLInput.vue
+++ resources/js/components/URLInput.vue
@@ -2,7 +2,7 @@
     <a class="text-black" v-if="previewUrl || url" :href="href" target="_blank">{{ label }}</a>
     <span v-else>{{ label }}</span>
 
-    <input v-model="url" type="text" class="form-control" required />
+    <input v-model="url" type="text" class="form-control" :required />
 </template>
 
 <script setup lang="ts">
@@ -18,5 +18,6 @@ const href = computed(() => {
 const props = defineProps<{
     previewUrl?: string;
     label: string;
+    required?: boolean;
 }>();
 </script>
